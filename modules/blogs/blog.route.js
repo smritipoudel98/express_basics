2ae46aa9b350d09b261b8a5b20c2049df845929d
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-13 17:40:52 +0545

    blog

--- modules/blogs/blog.route.js
+++ modules/blogs/blog.route.js
@@ -1,5 +1,39 @@
 const router = require("express").Router();
+
 router.get("/", (req, res) => {
-  res.json({ msg: "Hello from blogs!" });
+  const { limit, page, search } = req.query; //use for search,sorting and filter
+  //database opeartion
+  res.json({ msg: "Hello from get blogs" });
 });
+//ADD NEW USER
+router.post("/", (req, res) => {
+  console.log(req.body);
+  //database opeartion
+  res.json({ msg: "Hello from post blog" });
+});
+//UPDATE SINGLE USER FOR MORE THAN TWO FIELDS
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  console.log({ id, data });
+  //database opeartion
+  res.json({ msg: `Hello from put blogs${id}` });
+});
+//UPDATE SINGLE USER FOR ONE FIELD
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  console.log({ id, data });
+  //database opeartion
+  res.json({ msg: `Hello from patch blogs${id}` });
+});
+//DELETE SINGLE USER
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  console.log({ id, data });
+  //database opeartion
+  res.json({ msg: `Hello from delete blogs${id}` });
+});
+
 module.exports = router;
