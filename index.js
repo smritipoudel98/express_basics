feec4e74896176fc19e497c73b2689386932ad37
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-02-13 18:58:40 +0545

    error handling

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
 const express = require("express");
-const indexRouter = require("./routes");
-const app = express();
-app.use(express.json());
+const morgan = require("morgan");
 
+const indexRouter = require("./routes");
+const PORT = Number(process.env.PORT);
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/assests", express.static("public"));
 app.use("/", indexRouter);
-app.listen(8000, () => {
-  console.log("application is running");
+app.use((e, req, res, next) => {
+  e = e ? e.toString() : "Something went wrong";
+  res.status(500).json({ msg: e });
+});
+app.listen(PORT, () => {
+  console.log(`application is running${PORT}`);
 });
--- modules/blogs/blog.route.js
+++ modules/blogs/blog.route.js
@@ -1,39 +1,60 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  const { limit, page, search } = req.query; //use for search,sorting and filter
-  //database opeartion
-  res.json({ msg: "Hello from get blogs" });
+router.get("/", (req, res, next) => {
+  try {
+    const { limit, page, search } = req.query; //use for search,sorting and filter
+    //database opeartion
+    states;
+    res.json({ msg: "Hello from get blogs" });
+  } catch (e) {
+    next(e);
+  }
 });
 //ADD NEW USER
-router.post("/", (req, res) => {
-  console.log(req.body);
-  //database opeartion
-  res.json({ msg: "Hello from post blog" });
+router.post("/", (req, res, next) => {
+  try {
+    console.log(req.body);
+    //database opeartion
+    res.json({ msg: "Hello from post blog" });
+  } catch (e) {
+    next(e);
+  }
 });
 //UPDATE SINGLE USER FOR MORE THAN TWO FIELDS
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-  console.log({ id, data });
-  //database opeartion
-  res.json({ msg: `Hello from put blogs${id}` });
+router.put("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    console.log({ id, data });
+    //database opeartion
+    res.json({ msg: `Hello from put blogs${id}` });
+  } catch (e) {
+    next(e);
+  }
 });
 //UPDATE SINGLE USER FOR ONE FIELD
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-  console.log({ id, data });
-  //database opeartion
-  res.json({ msg: `Hello from patch blogs${id}` });
+router.patch("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    console.log({ id, data });
+    //database opeartion
+    res.json({ msg: `Hello from patch blogs${id}` });
+  } catch (e) {
+    next(e);
+  }
 });
 //DELETE SINGLE USER
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-  console.log({ id, data });
-  //database opeartion
-  res.json({ msg: `Hello from delete blogs${id}` });
+router.delete("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    console.log({ id, data });
+    //database opeartion
+    res.json({ msg: `Hello from delete blogs${id}` });
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
